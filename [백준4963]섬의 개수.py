10bd5c67dc811154d728d6fd70e4979502c58166
Seol_JY <wlsdud5654@gmail.com>
2023-01-13 12:04:29 +0900

    2023-01-13

---
+++ [백준4963]섬의 개수.py
@@ -0,0 +1,42 @@
+import sys
+sys.setrecursionlimit(10**6)
+def solution() -> None:
+    arr = []
+    idx = 0
+
+    def dfs(i: int, j: int, arr: list[list[int]]) -> None:
+        if not (0 <= i < len(arr)) or \
+            not (0 <= j < len(arr[0])) or \
+            not arr[i][j]:
+                return
+        arr[i][j] = 0
+
+        dfs(i + 1, j, arr)
+        dfs(i - 1, j, arr)
+        dfs(i, j + 1, arr)
+        dfs(i, j - 1, arr)
+        dfs(i - 1, j -  1, arr)
+        dfs(i - 1, j +  1, arr)
+        dfs(i + 1, j -  1, arr)
+        dfs(i + 1, j +  1, arr)
+
+    def numberOfIsland(arr: list[list[int]]) -> int:
+        count = 0
+        for i in range(len(arr)):
+            for j in range(len(arr[0])):
+                if arr[i][j]:
+                    dfs(i, j, arr)
+                    count += 1
+        return count
+
+    while True:
+        width, height =  map(int, sys.stdin.readline().rstrip().split())
+        if width == height == 0: break
+        tmp = []
+        for _ in range(height):
+            tmp.append(list(map(int, sys.stdin.readline().rstrip().split())))
+        arr.append(tmp[:])
+
+    for i in range(len(arr)): print(numberOfIsland(arr[i]))
+
+solution()
